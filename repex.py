54a9adc89ed904e67afee75ab54c1b950407316a
Silveira <anita_silveira@hotmail.com>
2019-01-16 14:18:58 -0500

    add distribute

--- repex.py
+++ repex.py
@@ -21,29 +21,33 @@ from yank.multistate import ReplicaExchangeSampler, MultiStateReporter
 
 
 class MyComposableState(GlobalParameterState):
-    lambda_restraints = GlobalParameterState.GlobalParameter('lambda_restraints', standard_value=1.0)
-    K_parallel = GlobalParameterState.GlobalParameter('K_parallel',
-                                                    standard_value=1000*unit.kilojoules_per_mole/unit.nanometer**2)
-    Kmax = GlobalParameterState.GlobalParameter('Kmax',
-                                                standard_value=1000*unit.kilojoules_per_mole/unit.nanometer**2)
-    Kmin = GlobalParameterState.GlobalParameter('Kmin',
-                                                standard_value=10*unit.kilojoules_per_mole/unit.nanometer**2)
+     lambda_restraints = GlobalParameterState.GlobalParameter('lambda_restraints', standard_value=1.0)
+     K_parallel = GlobalParameterState.GlobalParameter('K_parallel',
+                                                     standard_value=1000*unit.kilojoules_per_mole/unit.nanometer**2)
+     Kmax = GlobalParameterState.GlobalParameter('Kmax',
+                                                 standard_value=1000*unit.kilojoules_per_mole/unit.nanometer**2)
+     Kmin = GlobalParameterState.GlobalParameter('Kmin',
+                                                 standard_value=10*unit.kilojoules_per_mole/unit.nanometer**2)
 
-# def write_cv(replica_index, context, simulation):
-#
-#         state_index = simulation._replica_thermodynamic_states[replica_index]
-#         ss = simulation.sampler_states[replica_index]
-#         ss.apply_to_context(context, ignore_velocities=True)
-#         ss.update_from_context(context, ignore_velocities=True)
-#         print("{} {} {}".format(state_index , *ss.collective_variables),file=file)
+def write_cv(replica_index, context, simulation, files):
+
+        state_index = simulation._replica_thermodynamic_states[replica_index]
+        ss = simulation.sampler_states[replica_index]
+        ss.apply_to_context(context, ignore_velocities=True)
+        ss.update_from_context(context, ignore_positions=True, ignore_velocities=True)
+        print("{} {}".format(*ss.collective_variables),file=files[state_index])
+
 
 def main():
     parser = argparse.ArgumentParser(description='Compute a potential of mean force (PMF) for porin permeation.')
     parser.add_argument('--index', dest='index', action='store', type=int,
                     help='Index of ')
+    parser.add_argument('--output', dest='output_filename', action='store', default='output.nc',
+                    help='output netcdf filename (default: output.nc)')
 
     args = parser.parse_args()
     index = args.index
+    output_filename = args.output_filename
 
     logger = logging.getLogger(__name__)
     logging.root.setLevel(logging.DEBUG)
@@ -92,16 +96,17 @@ def main():
                                     reassign_velocities=False)
     simulation = ReplicaExchangeSampler(mcmc_moves=move, number_of_iterations=1)
     analysis_particle_indices = topology.select('(protein and mass > 3.0) or (resname MER and mass > 3.0)')
-    reporter = MultiStateReporter('output_' + str(index) + '.nc',checkpoint_interval=2000, analysis_particle_indices=analysis_particle_indices)
+    reporter = MultiStateReporter(output_filename,checkpoint_interval=2000, analysis_particle_indices=analysis_particle_indices)
 
-    first, last = limits[index]	
+    first, last = limits[index]
     # Initialize compound thermodynamic states
     protocol = {'lambda_restraints': [ i/159 for i in range(first, last+1)],
                 'K_parallel': [1250*unit.kilojoules_per_mole/unit.nanometer**2 for i in range(first, last+1)],
                 'Kmax': [500*unit.kilojoules_per_mole/unit.nanometer**2 for i in range(first, last+1)],
                 'Kmin': [500*unit.kilojoules_per_mole/unit.nanometer**2 for i in range(first, last+1)]}
 
-    my_composable_state = MyComposableState()
+    my_composable_state = MyComposableState.from_system(openmm_system)
+    logger.debug(my_composable_state.lambda_restraints)
     compound_states = states.create_thermodynamic_state_protocol(thermodynamic_state_deserialized,
                                                                 protocol=protocol,
                                                                 composable_states=[my_composable_state])
@@ -111,34 +116,21 @@ def main():
                       storage=reporter)
 
     simulation.equilibrate(100000, mcmc_moves=move)
-    # Run the simulation
+
     simulation.run()
     ts = simulation._thermodynamic_states[0]
     context, _ = openmmtools.cache.global_context_cache.get_context(ts)
 
-    files_names = ['state_{}_{}.log'.format(index, i) for i in range(simulation.n_replicas)]
+    files_names = ['state_{}_{}.log'.format(index, i) for i in range(first, last+1)]
     files = []
     for i, file in enumerate(files_names):
         files.append(open(file, 'w'))
 
-    for replica_index in range(simulation.n_replicas):
-        state_index = simulation._replica_thermodynamic_states[replica_index]
-        ss = simulation.sampler_states[replica_index]
-        ss.apply_to_context(context, ignore_velocities=True)
-        ss.update_from_context(context, ignore_velocities=True)
-        print("{} {} {}".format(state_index , *ss.collective_variables),file=files[replica_index])
-
-    #mpi.distribute(write_cv, range(simulation.n_replicas), context, simulation,  send_results_to=None)
+    mpi.distribute(write_cv, range(simulation.n_replicas), context, simulation, files, send_results_to=None)
 
     for i in range(10):
        simulation.extend(n_iterations=2)
-       for replica_index in range(simulation.n_replicas):
-           state_index = simulation._replica_thermodynamic_states[replica_index]
-           ss = simulation.sampler_states[replica_index]
-           ss.apply_to_context(context, ignore_velocities=True)
-           ss.update_from_context(context, ignore_velocities=True)
-           print("{} {} {}".format(state_index , *ss.collective_variables),file=files[replica_index])
-       #mpi.distribute(write_cv, range(simulation.n_replicas), context, simulation, send_results_to=None)
+       mpi.distribute(write_cv, range(simulation.n_replicas), context, simulation, files, send_results_to=None)
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
